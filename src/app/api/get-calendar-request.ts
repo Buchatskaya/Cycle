af3a68ff686e5bd9ecd1dcad1af912e4fb31396e
tbuchatskaya <tg.buchatskaya@gmail.com>
2018-06-03 13:00:21 +0300

    API message classes

--- src/app/api/get-calendar-request.ts
+++ src/app/api/get-calendar-request.ts
@@ -1,2 +1,21 @@
 export class GetCalendarRequest {
+  readonly monthStartField = 'monthStart';
+  readonly monthCountField = 'monthCount';
+
+  private _monthStart: Date;
+  private _monthCount: number;
+
+
+  constructor(monthStart: Date, monthCount: number) {
+    this._monthStart = monthStart;
+    this._monthCount = monthCount;
+  }
+
+  get monthStart(): Date {
+    return this._monthStart;
+  }
+
+  get monthCount(): number {
+    return this._monthCount;
+  }
 }
--- src/app/api/put-profile-request.ts
+++ src/app/api/put-profile-request.ts
@@ -1,2 +1,21 @@
 export class PutProfileRequest {
+  readonly nameField = 'name';
+  readonly periodField = 'period';
+
+  private _name: string;
+  private _period: number;
+
+
+  constructor(name: string, period: number) {
+    this._name = name;
+    this._period = period;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get period(): number {
+    return this._period;
+  }
 }
